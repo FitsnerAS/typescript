c3c934c00f19c436584025043363a7af9021b801
Aliaksandr Fitsner <aliaksandr_fitsner@epam.com>
2016-11-28 17:35:06 +0300

    dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,11 @@
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var webpack = require('webpack');
+var HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
         context: __dirname + '/app',
         entry: './index.ts',
         output: {
-                path: __dirname + '/app',
+                path: __dirname + '/dist',
                 filename: 'bundle.js'
         },
         devServer: {
@@ -26,16 +27,19 @@ module.exports = {
                         },
                         {
                                 test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                                loader: "url-loader?limit=10000&minetype=application/font-woff&name=assets/fonts/[name].[ext]"
+                                loader: "url-loader?limit=10000&minetype=application/font-woff&name=fonts/[name].[ext]"
                         },
                         {
                                 test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
-                                loader: "file-loader?limit=10000&name=assets/fonts/[name].[ext]"
+                                loader: "file-loader?limit=10000&name=fonts/[name].[ext]"
                         }
                 ]
         },
         plugins: [
                 new ExtractTextPlugin("bundle.css"),
-                new webpack.optimize.UglifyJsPlugin()
+                new webpack.optimize.UglifyJsPlugin(),
+                new HtmlWebpackPlugin({
+                        template: 'index.html'
+                }),
         ]
 };
